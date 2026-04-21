c12369bd12bf13dcdd4b1efaf609a48d4e1f4255
Ashwin Vats <av18011998@gmail.com>
2026-04-21 20:24:51 +0530

    feat: add dev vpc configuration

--- dev/main.tf
+++ dev/main.tf
@@ -5,3 +5,29 @@ module "backend" {
   dynamodb_table_name = "ashwin-dev-terraform-locks"
   kms_key_alias       = "ashwin-dev-tf-key"
 }
+
+module "vpc" {
+  source = "../../infra-modules/vpc"
+
+  env = "dev"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  azs = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24"
+  ]
+}
