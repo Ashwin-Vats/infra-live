92f7f9b0e35e8ef6c2c830eae6792ccdd2f6e837
Ashwin Vats <av18011998@gmail.com>
2026-04-21 20:18:03 +0530

    adding terraform init plugins

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket         = "ashwin-dev-terraform-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "ashwin-dev-terraform-locks"
+    encrypt        = true
+  }
 }
