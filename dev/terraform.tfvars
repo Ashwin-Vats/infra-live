24bf7def6d3d18b017794ac4037198bd88614bf6
Ashwin Vats <av18011998@gmail.com>
2026-04-23 01:53:12 +0530

    adding variavles and values

---
+++ dev/terraform.tfvars
@@ -0,0 +1,9 @@
+env = "dev"
+
+region = "us-east-1"
+
+domain_name = "chart-example.local"
+
+cluster_name = "dev.k8s.local"
+
+bastion_key_name = "dev-bastion-key"
---
+++ dev/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "bastion_key_name" {
+  type = string
+}
