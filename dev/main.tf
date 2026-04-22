99c037b7053b6d3059a705fc439be3a2018f7f1d
Ashwin Vats <av18011998@gmail.com>
2026-04-23 01:35:12 +0530

    adding executable modules

--- dev/main.tf
+++ dev/main.tf
@@ -73,8 +73,88 @@ module "kops_s3" {
   env = "dev"
 
   account_id = "d125788629837"
-
-  region = "ap-south-1"
-
+  region     = "ap-south-1"
   kms_key_arn = module.kms.kms_key_arn
+}
+
+# module "dns" {
+#   source = "../../infra-modules/dns"
+
+#   env = "dev"
+
+#   domain_name = "corp.example.internal"
+
+#   vpc_id = module.vpc.vpc_id
+# }
+
+
+module "kops_oidc" {
+  source = "../../infra-modules/kops-oidc"
+
+  env = "dev"
+
+  cluster_name = "kops.dev.corp.example.internal"
+}
+
+#########################################
+# Cluster IAM
+#########################################
+
+module "cluster_iam" {
+  source = "../../infra-modules/cluster-iam"
+
+  env = "dev"
+
+  cluster_name     = "kops.dev.corp.example.internal"
+  oidc_provider_arn = module.kops_oidc.oidc_provider_arn
+  oidc_provider_url = module.kops_oidc.oidc_provider_url
+}
+
+#########################################
+# Node IAM
+#########################################
+
+module "node_iam" {
+  source = "../../infra-modules/node-iam"
+
+  env = "dev"
+}
+
+#########################################
+# ArgoCD IAM
+#########################################
+
+module "argocd_iam" {
+
+  source = "../../infra-modules/argocd-iam"
+  env = "dev"
+  oidc_provider_arn =    module.kops_oidc.oidc_provider_arn
+
+  oidc_provider_url =    module.kops_oidc.oidc_provider_url
+}
+
+#########################################
+# Addons IRSA
+#########################################
+
+module "addons_irsa" {
+  source = "../../infra-modules/addons-irsa"
+
+  env = "dev"
+
+  oidc_provider_arn =    module.kops_oidc.oidc_provider_arn
+
+  oidc_provider_url =    module.kops_oidc.oidc_provider_url
+}
+
+#########################################
+# ECR
+#########################################
+
+module "ecr" {
+  source = "../../infra-modules/ecr"
+
+  env = "dev"
+
+  repository_name = "app"
 }
